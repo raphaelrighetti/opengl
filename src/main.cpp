3ccf830d6bd8579f2847daa39c241234476856a5
raphaelrighetti <raphaelrighettiramos@hotmail.com>
2026-09-10 11:48:16 -0300

    renderizando triangulo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,111 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <Windows.h>
+
+std::string ReadFile(const char* path)
+{
+	std::ifstream file{ path };
+
+	if (!file.is_open())
+	{
+		std::cerr << "Arquivo não abriu..." << std::endl;
+		return "";
+	}
+
+	std::string content{ std::istreambuf_iterator<char>(file),
+		std::istreambuf_iterator<char>() };
+
+	file.close();
+	std::cout << content << std::endl;
+
+	std::cout << content << std::endl;
+
+	return content;
+}
+
+GLuint CreateProgram()
+{
+	std::string vsc{ ReadFile("../shaders/basic.vert") };
+	std::string fsc{ ReadFile("../shaders/basic.frag") };
+	const GLchar* vShaderCode[]{ vsc.c_str() };
+	const GLchar* fShaderCode[]{ fsc.c_str() };
+	std::cout << *vShaderCode << std::endl;
+	std::cout << *fShaderCode << std::endl;
+
+	GLuint program{ glCreateProgram() };
+
+	GLuint vShader{ glCreateShader(GL_VERTEX_SHADER) };
+	glShaderSource(vShader, 1, vShaderCode, nullptr);
+	GLuint fShader{ glCreateShader(GL_FRAGMENT_SHADER) };
+	glShaderSource(fShader, 1, fShaderCode, nullptr);
+
+	GLint success{};
+	GLchar log[1024]{};
+
+	glCompileShader(vShader);
+	glGetShaderiv(vShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vShader, 1024, NULL, log);
+		std::cout << "Deu merda na vShader..." << std::endl;
+		std::cout << log << std::endl;
+		return 0;
+	}
+	glCompileShader(fShader);
+	glGetShaderiv(fShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fShader, 1024, NULL, log);
+		std::cout << "Deu merda na fShader..." << std::endl;
+		std::cout << log << std::endl;
+		return 0;
+	}
+
+	glAttachShader(program, vShader);
+	glAttachShader(program, fShader);
+
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(program, 1024, NULL, log);
+		std::cout << "Falhou em linkar o program..." << std::endl;
+		std::cout << log << std::endl;
+	}
+
+	glDeleteShader(vShader);
+	glDeleteShader(fShader);
+
+	return program;
+}
+
+GLuint CreateTriangle()
+{
+	GLfloat vertices[]{
+		-1.0, -1.0, 0.0,
+		1.0, -1.0, 0.0,
+		0.0, 1.0, 0.0
+	};
+
+	GLuint VAO, VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	return VAO;
+}
 
 static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -10,6 +115,8 @@ static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 
 int main()
 {
+	SetConsoleOutputCP(CP_UTF8);
+
 	if (!glfwInit()) {
 		std::cerr << "Failed to initialize GLFW\n";
 		return -1;
@@ -26,7 +133,7 @@ int main()
 
 	const int width = 800;
 	const int height = 600;
-	GLFWwindow* window = glfwCreateWindow(width, height, "OpenGL Window", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "Raphinha games", NULL, NULL);
 	if (!window) {
 		std::cerr << "Failed to create GLFW window\n";
 		glfwTerminate();
@@ -54,6 +161,9 @@ int main()
 	glfwGetFramebufferSize(window, &fbWidth, &fbHeight);
 	glViewport(0, 0, fbWidth, fbHeight);
 
+	GLuint triangleVAO{ CreateTriangle()};
+	GLuint program{ CreateProgram() };
+
 	// Main loop
 	while (!glfwWindowShouldClose(window)) {
 		// Input: close on ESC
@@ -61,14 +171,20 @@ int main()
 			glfwSetWindowShouldClose(window, true);
 			std::cout << "ESC pressed, closing window.\n";
 		}
-		glfwPollEvents();
 
 		// Render
 		glClearColor(0.1f, 0.15f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glUseProgram(program);
+		glBindVertexArray(triangleVAO);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		// Swap buffers and poll events
 		glfwSwapBuffers(window);
+
+		glfwPollEvents();
 	}
 
 	glfwDestroyWindow(window);
